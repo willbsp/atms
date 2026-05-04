bd3222eeba9fb2ddcd9d7117476366fbd2eeffd5
Will Spooner <contact@willbspooner.uk>
2026-05-04 19:02:59 +0100

    Add tmux methods

--- tmux/tmux.go
+++ tmux/tmux.go
@@ -2,20 +2,53 @@ package tmux
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
-// TODO what if not in a session?
 func ListSessions() []string {
 	o, _ := tmuxCmd("list-sessions", "-F", "#{session_name}")
-	lines := strings.Split(strings.TrimSpace(string(o)), "\n")
+	if o == "" {
+		return []string{}
+	}
+	lines := strings.Split(o, "\n")
 	sessions := make([]string, len(lines))
 	for i, l := range lines {
-		l = strings.TrimSpace(l)
-		sessions[i] = l
+		sessions[i] = strings.TrimSpace(l)
 	}
 	return sessions
+}
+
+func SwitchOrAttach(path string) error {
+	name := SessionName(path)
+	if !HasSession(name) {
+		if o, err := tmuxCmd("new-session", "-d", "-s", name, "-c", path); err != nil {
+			return fmt.Errorf("failed to create session %q: %s (%w)", name, o, err)
+		}
+	}
+	if IsInsideTmux() {
+		_, err := tmuxCmd("switch-client", "-t", name)
+		return err
+	}
+
+	_, err := tmuxCmd("attach-session", "-t", name)
+	return err
+}
+
+func SessionName(repoPath string) string {
+	name := filepath.Base(repoPath)
+	return name
+}
+
+func IsInsideTmux() bool {
+	return os.Getenv("TMUX") != ""
+}
+
+func HasSession(name string) bool {
+	cmd := exec.Command("tmux", "has-session", "-t", name)
+	return cmd.Run() == nil
 }
 
 func tmuxCmd(args ...string) (string, error) {
